2b879c74580ab714e9a005ae83ca96c7f2779a86
james-burgess <jms.bgr@gmail.com>
2017-02-11 18:54:36 +0000

    a lot for a button and a waiter

--- button.py
+++ button.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python 
 
 import RPi.GPIO as GPIO
-
+import time
 GPIO.setmode(GPIO.BCM)
 
 
@@ -9,11 +9,13 @@ GPIO.setup(18, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
 
 try:
+
   while True:
 
     input_state = GPIO.input(18)
     if input_state == False:
       print("you pressed a button")
+      time.sleep(2)
 
 
 
