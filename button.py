c86c15ea1e6ac89467cc896b3a6a87add5674875
james-burgess <jms.bgr@gmail.com>
2017-02-11 21:07:11 +0200

    now its even switchier

--- button.py
+++ button.py
@@ -8,14 +8,20 @@ GPIO.setup(18, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 GPIO.setup(2,GPIO.OUT)
 GPIO.output(2,GPIO.HIGH)
 
+def switch:
+	if GPIO.output(2) == GPIO.HIGH:
+		GPIO.output(2,GPIO.LOW)
+	else
+		GPIO.output(2,GPIO.HIGH)
+
 try:
   while True:
     input_state = GPIO.input(18)
     if input_state == False:
       print("you pressed a button")
-      GPIO.output(2,GPIO.LOW)
+      switch()
       time.sleep(2)
-      GPIO.output(2,GPIO.HIGH)
+      
 except KeyboardInterrupt:
   print ("quitting...")
   GPIO.cleanup()
