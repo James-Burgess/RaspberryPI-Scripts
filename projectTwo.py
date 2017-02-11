30fa44376adea18b4f3921ea486868ac1bbdb630
james-burgess <jms.bgr@gmail.com>
2017-02-11 17:18:02 +0200

    new ligh switcher

---
+++ projectTwo.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+
+import explorerhat
+import math
+import time
+
+lr = explorerhat.light.red
+lb = explorerhat.light.blue
+ly = explorerhat.light.yellow
+lg = explorerhat.light.green
+
+while True:
+	lg(on)
+	time.sleep(.5)
+	lb(on)
+	time.sleep(.5)
+	ly(on)
+	time.sleep(.5)
+	lg(on)
+	time.sleep(.5)
+
+	lg(off)
+	time.sleep(.5)
+	lb(off)
+	time.sleep(.5)
+	ly(off)
+	time.sleep(.5)
+	lg(off)
+	time.sleep(.5)
