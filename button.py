a853035ce6041e106e9025f9fa9bd772f8d933b0
james-burgess <jms.bgr@gmail.com>
2017-02-11 21:00:15 +0200

    now its a switch

--- button.py
+++ button.py
@@ -2,25 +2,20 @@
 
 import RPi.GPIO as GPIO
 import time
+
 GPIO.setmode(GPIO.BCM)
-
-
 GPIO.setup(18, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-
+GPIO.setup(2,GPIO.OUT)
+GPIO.output(2,GPIO.HIGH)
 
 try:
-
   while True:
-
     input_state = GPIO.input(18)
     if input_state == False:
       print("you pressed a button")
+      GPIO.output(2,GPIO.LOW)
       time.sleep(2)
-
-
-
-
-
+      GPIO.output(2,GPIO.HIGH)
 except KeyboardInterrupt:
   print ("quitting...")
   GPIO.cleanup()
